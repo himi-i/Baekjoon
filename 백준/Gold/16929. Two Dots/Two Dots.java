deec42460996fd567780615335d37fc6a0a972a7
himi-i <114150749+himi-i@users.noreply.github.com>
2025-10-15 23:23:50 +0900

    [Gold IV] Title: Two Dots, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static char[][] board;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static boolean hasCycle = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        board = new char[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            board[i] = line.toCharArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j, -1, -1, board[i][j]);
+                    if (hasCycle) {
+                        System.out.println("Yes");
+                        return;
+                    }
+                }
+            }
+        }
+
+        System.out.println("No");
+    }
+
+    static void dfs(int x, int y, int px, int py, char color) {
+        visited[x][y] = true;
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (board[nx][ny] != color) continue;
+
+            if (!visited[nx][ny]) {
+                dfs(nx, ny, x, y, color);
+            } else if (!(nx == px && ny == py)) {
+                hasCycle = true;
+                return;
+            }
+        }
+    }
+}
