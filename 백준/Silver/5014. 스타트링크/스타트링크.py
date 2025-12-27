76c04daaed03d52920ad733f05b838d54bc2e317
himi-i <114150749+himi-i@users.noreply.github.com>
2025-12-27 20:01:19 +0900

    [Silver I] Title: 스타트링크, Time: 476 ms, Memory: 73868 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,23 +1,27 @@
+import sys
 from collections import deque
 
-F, S, G, U, D = map(int, input().split())
-visited = [-1] * (F + 1)
+input = sys.stdin.readline
 
-q = deque([S])
-visited[S] = 0 
-while q:
-    x = q.popleft()
+def main():
+    F, S, G, U, D = map(int, input().split())
 
-    if x == G:
-        break
-    for nx in(x + U, x - D):
-        if 1 <= nx <= F and visited[nx] == -1:
-            visited[nx] = visited[x] + 1
-            q.append(nx)
+    q = deque([S])
+    visited = [-1] * (F + 1)
+    visited[S] = 0
+    while q:
+        x = q.popleft()
+        if x == G:
+            break
+        for nx in (x + U, x - D):
+            if(1 <= nx <= F and visited[nx] == -1):
+                visited[nx] = visited[x] + 1
+                q.append(nx)
 
-if visited[G] == -1:
-    print("use the stairs")
-else:
-    print(visited[G])
+    if visited[G] == -1:
+        print("use the stairs")
+    else:
+        print(visited[G])
 
-
+if __name__ == "__main__":
+    main()
