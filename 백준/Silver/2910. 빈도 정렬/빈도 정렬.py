c8b37d7a7680f8b34d1adfe3457058a1c89501dc
himi-i <114150749+himi-i@users.noreply.github.com>
2026-01-29 18:59:36 +0900

    [Silver III] Title: 빈도 정렬, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,22 +1,23 @@
-from heapq import heappop, heappush
 import sys
 input = sys.stdin.readline
- 
-n, c = map(int, input().split())
-arr = list(map(int, input().split()))
- 
-dict = {}
-for i in range(n):
-    if arr[i] not in dict:
-        dict[arr[i]] = [i, 1]
+
+N, C = map(int, input().split())
+nums = list(map(int, input().split()))
+
+count = {}
+order = {}
+
+idx = 0
+for x in nums:
+    if x not in count:
+        count[x] = 1
+        order[x] = idx
+        idx += 1
     else:
-        dict[arr[i]][1] += 1
- 
-hq = []
-for k, v in dict.items():
-    heappush(hq, [-v[1], v[0], k])
- 
-while hq:
-    cnt, idx, num = heappop(hq)
-    for _ in range(-cnt):
-        print(num, end=" ")
+        count[x] += 1
+
+sorted_nums = sorted(count.items(),
+                     key=lambda x: (-x[1], order[x[0]]))
+
+for num, cnt in sorted_nums:
+    print((str(num) + ' ') * cnt, end='')
