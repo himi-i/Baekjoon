76574329ba8ca363fddd5f83ac65163fded314cf
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-11 21:03:07 +0900

    [level 3] Title: 단속카메라, Time: 0.92 ms, Memory: 9.56 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    
+    routes.sort(key=lambda x: x[1])
+    
+    camera = -30001 
+    answer = 0
+    
+    for start, end in routes:
+        if camera < start:
+            answer += 1
+            camera = end  
+    
+    return answer
