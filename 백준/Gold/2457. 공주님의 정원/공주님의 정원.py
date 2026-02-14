118bcf6a0ed570cf1c25d2382a110cf725df1598
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-14 19:06:33 +0900

    [Gold III] Title: 공주님의 정원, Time: 260 ms, Memory: 60356 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -10,26 +10,24 @@ for _ in range(N):
 
 flower.sort(key=lambda x: (x[0][0], x[0][1]))
 
-current = (3, 1)        
-target = (11, 30)       
-idx = 0
+current = (3, 1)
+target = (11, 30)
 count = 0
+idx = 0
 
 while current <= target:
     max_end = current
-    found = False
 
     while idx < N and flower[idx][0] <= current:
         if flower[idx][1] > max_end:
             max_end = flower[idx][1]
-            found = True
         idx += 1
 
-    if not found:
+    if max_end == current:
         print(0)
-        sys.exit()
+        break
 
     count += 1
     current = max_end
-
-print(count)
+else:
+    print(count)
