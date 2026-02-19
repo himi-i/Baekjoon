b30898ef3398927b089e0563d0dfd2ab7cf21062
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-19 14:16:46 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 588 ms, Memory: 52144 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+dp = [ [0] * n for _ in range(n)]
+
+result = 0
+def dfs(x, y):
+
+
+    if dp[x][y] != 0:
+        return dp[x][y]
+    
+    dp[x][y] = 1
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] > graph[x][y]:
+                dp[x][y] = max(dp[x][y], dfs(nx,ny) + 1)
+
+    return dp[x][y]
+
+for i in range(n):
+    for j in range(n):
+        result = max(dfs(i, j), result)
+
+print(result)
+
