d89d5ce663f41f693db1bf849b31f75f1e761a68
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-21 23:31:27 +0900

    [Silver II] Title: 쇠막대기, Time: 60 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+
+stack = []
+result = 0
+
+for i in range(len(s)):
+    if s[i] == "(":
+        stack.append("(")
+    else:  
+        stack.pop()
+
+        if s[i - 1] == "(":
+            result += len(stack)
+        else:
+            result += 1
+
+print(result)
