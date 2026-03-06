e5293e6be51bc20a5626250836dec9a97e79fa37
himi-i <114150749+himi-i@users.noreply.github.com>
2026-03-07 00:57:10 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,28 +2,24 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-maze = [list(map(int, input().strip())) for _ in range(N)]
+n, m = map(int, input().split())
+board = [list(map(int, input().strip())) for _ in range(n)]
 
-dist = [[0] * M for _ in range(N)]
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
 q = deque()
-q.append((0, 0))
-dist[0][0] = 1
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+q.append((0,0))
 
 while q:
-    x, y = q.popleft()
-    
+    x,y = q.popleft()
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        
-        if 0 <= nx < N and 0 <= ny < M:
-            if maze[nx][ny] == 1 and dist[nx][ny] == 0:
-                dist[nx][ny] = dist[x][y] + 1
-                q.append((nx, ny))
 
-print(dist[N-1][M-1])
+        if 0<=nx<n and 0<=ny<m and board[nx][ny] == 1:
+            board[nx][ny] = board[x][y] + 1
+            q.append((nx,ny))
+
+print(board[n-1][m-1])
